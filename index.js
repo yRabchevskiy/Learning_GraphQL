ccc0ace06e70f9cf1f07c94cbcbebd9c6dbd5cc7
Yurii Rabchevskiy <yrabchevskiy@gmail.com>
2024-03-14 17:38:45 +0200

    update example server

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { ApolloServer } = require('apollo-server')
 
 const users = [];
+let _id = 0;
 
 const typeDefs = `
+    enum SexType {
+        MALE
+        FEMALE
+    }
     type User {
+        id: ID!
         name: String!
         age: Int!
+        sexType: SexType!
+    }
+    input PostUserInput {
+        name: String!
+        age: Int!
+        sexType: SexType=MALE
     }
     type Query {
         totalUsers: Int!
         getUsers: [User]
     }
     type Mutation {
-        addUser(name: String! age: Int!): Boolean!
+        addUser(input: PostUserInput!): User!
     }
 `
 
@@ -23,10 +35,22 @@ const resolvers = {
     },
     Mutation: {
         addUser(parent, args) {
-            users.push(args);
-            return true;
+            console.log(args)
+            let user = {
+                id: _id++,
+                ...args.input
+            }
+            users.push(user);
+            return user;
         }
     }
+
+    /**
+     * Тривіальний розпізнавач
+     * Photo: {
+     *  url: parent => `http://somesite.com/img/${parent.id}.jpg`
+     * }
+     */
 }
 
 const server = new ApolloServer({
