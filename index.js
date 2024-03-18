e389f7d193e967bd9f0a2214417b7c010127bc66
Yurii Rabchevskiy <yrabchevskiy@gmail.com>
2024-03-18 10:11:59 +0200

    learn scalar

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server');
+const { GraphQLScalarType } = require('graphql');
 
-const users = [];
-let _id = 0;
+const users = [
+    { id: 1, name: "Yura", age: 35, rang: "SOLDIER" },
+    { id: 2, name: "Oksana", age: 37, rang: "SOLDIER" },
+    { id: 3, name: "Yurii", age: 38, rang: "OFFICER" }
+];
+const tasks = [
+    { id: 1, name: "Teach", start: '2007-12-03T10:15:30Z', end: '2007-12-03T10:15:30Z', category: "STUDY", ownerId: 1 },
+    { id: 2, name: "Test", start: '2007-12-03T10:15:30Z', end: '2007-12-03T10:15:30Z', category: "WORK", ownerId: 2 },
+    { id: 3, name: "Develop", start: '2007-12-03T10:15:30Z', end: '2007-12-03T10:15:30Z', category: "STUDY", ownerId: 3 },
+    { id: 4, name: "Eat", start: '2007-12-03T10:15:30Z', end: '2007-12-03T10:15:30Z', category: "WORK", ownerId: 1 },
+];
+let _userId = 0;
+let _taskId = 0;
 
 const typeDefs = `
-    enum SexType {
-        MALE
-        FEMALE
+    enum RangType {
+        SOLDIER
+        OFFICER
+    }
+    enum TaskCategoryType {
+        WORK
+        STUDY
+    }
+    scalar DateTime
+    type Task {
+        id: ID!
+        name: String!
+        start: DateTime!
+        end: DateTime!
+        category: TaskCategoryType!
+        createdBy: User!
     }
     type User {
         id: ID!
         name: String!
         age: Int!
-        sexType: SexType!
+        rang: RangType!
+        tasks: [Task!]!
     }
     input PostUserInput {
         name: String!
         age: Int!
-        sexType: SexType=MALE
+        rang: RangType=SOLDIER
     }
     type Query {
         totalUsers: Int!
         getUsers: [User]
+        getTasks: [Task]
+        getUserById(id: Int!): User!
     }
     type Mutation {
         addUser(input: PostUserInput!): User!
@@ -31,19 +59,40 @@ const typeDefs = `
 const resolvers = {
     Query: {
         totalUsers: () => users.length,
-        getUsers: () => users
+        getUsers: () => users,
+        getTasks: () => tasks,
+        getUserById: (parent, { id }) => {
+            return users.find(u => u.id === id)
+        }
     },
     Mutation: {
         addUser(parent, args) {
             console.log(args)
             let user = {
-                id: _id++,
+                id: _userId++,
                 ...args.input
             }
             users.push(user);
             return user;
         }
-    }
+    },
+    User: {
+        tasks: parent => {
+            return tasks.filter(u => u.ownerId === parent.id);
+        }
+    },
+    Task: {
+        createdBy: parent => {
+            return users.find(t => t.id === parent.ownerId);
+        }
+    },
+    DateTime: new GraphQLScalarType({
+        name: "DateTime",
+        description: 'Valid datetime',
+        parseValue: value => new Date(value),
+        parseLiteral: data => data.value,
+        serialize: value => new Date(value).toISOString()
+    })
 
     /**
      * Тривіальний розпізнавач
